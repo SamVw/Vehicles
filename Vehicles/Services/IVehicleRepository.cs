b1e31094271dcd7dff89e7817ab0cfa92efc0f74
sam vanwelsenaere <sam.vanwelsenaere@hotmail.com>
2020-11-30 13:40:48 +0100

    Created vehicle repository interface

---
+++ Vehicles/Services/IVehicleRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vehicles.Models;
+
+namespace Vehicles.Services
+{
+    public interface IVehicleRepository
+    {
+        IEnumerable<Vehicle> GetAll();
+        Vehicle Get(int id);
+        void Add(Vehicle vehicle);
+        void Delete(Vehicle vehicle);
+        void Update(Vehicle vehicle);
+    }
+}
