250fcb6a137f29a1b800d1801873dca4d56e7516
sam vanwelsenaere <sam.vanwelsenaere@hotmail.com>
2020-11-30 13:37:23 +0100

    Created HomeController + models

---
+++ Vehicles/Models/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public string VIN { get; set; }
+
+        public VehicleTypeEnum Type { get; set; }
+
+        public ColorEnum Color { get; set; }
+    }
+}
---
+++ Vehicles/Models/VehicleTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.Models
+{
+    public enum VehicleTypeEnum
+    {
+        Truck,
+        Bus,
+        Car,
+        Suv
+    }
+}
