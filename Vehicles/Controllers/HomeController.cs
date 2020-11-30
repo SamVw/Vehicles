c47e0d7393a743d9c63ad9c9a201fbe13fcf9288
sam vanwelsenaere <sam.vanwelsenaere@hotmail.com>
2020-11-30 14:36:22 +0100

    Add Post route + implementation

--- Vehicles/Controllers/HomeController.cs
+++ Vehicles/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Vehicles.Services;
+using Vehicles.ViewModels;
 
 namespace Vehicles.Controllers
 {
@@ -11,7 +13,20 @@ namespace Vehicles.Controllers
     [ApiController]
     public class HomeController : ControllerBase
     {
+        private readonly IVehicleRepository _vehicleRepository;
+
+        public HomeController(IVehicleRepository vehicleRepository)
+        {
+            this._vehicleRepository = vehicleRepository;
+        }
+
         [HttpGet]
         public void Test() { }
+
+        [HttpPost]
+        public void Post(PostVehicleVM vehicle)
+        {
+            _vehicleRepository.Add(vehicle.ToModel());
+        }
     }
 }
---
+++ Vehicles/ViewModels/PostVehicleVM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Vehicles.Models;
+
+namespace Vehicles.ViewModels
+{
+    public class PostVehicleVM
+    {
+        [Required]
+        public string Make { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public string Vin { get; set; }
+        public int Type { get; set; }
+        public int Color { get; set; }
+
+        public Vehicle ToModel()
+        {
+            return new Vehicle
+            {
+                Make = this.Make,
+                Model = this.Model,
+                VIN = this.Vin,
+                Type = (VehicleTypeEnum)this.Type,
+                Color = (ColorEnum)this.Color
+            };
+        }
+    }
+}
